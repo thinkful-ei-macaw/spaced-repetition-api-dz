bc37996908551cd7ecf524dcf8e84332f9f5a0ad
Desmond Wareham <desmond.wareham@gmail.com>
2020-06-18 14:47:53 -0400

    updated error handler

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,6 +1,7 @@
 const { NODE_ENV } = require('../config')
 
 module.exports = function errorHandler(error, req, res, next) {
+  (console.log(error),{error: error.message, details:error})
   const response = (NODE_ENV === 'production')
     ? { error: 'Server error' }
     : (console.error(error), { error: error.message, details: error })
